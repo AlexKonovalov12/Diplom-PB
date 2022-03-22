4dd17d7a9501cf9190504d65e9e8dd2a157cb511
Alexander Konovalov <92515926+AlexKonovalov12@users.noreply.github.com>
2022-03-22 14:08:54 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,50 +1,43 @@
 let players = ['x', 'o'];
 let activePlayer = 0;
 let count = 0;
-let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-];
+let board;
 
 function startGame() {
-    let board = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-    renderBoard(board);
-    count = 0;
+  board = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+  count = 0;
+  renderBoard(board);
 };
 
 function checkWin() {
-    let tap = players[activePlayer];
-    for (let i = 0; i <= 2; i++) {
-        for (let j = 0; j <= 2; j++) {
-            if (board[i][0] === tap && board[i][1] === tap && board[i][2] === tap) {
-                showWinner(activePlayer);
-            } else if (board[0][j] === tap && board[1][j] === tap && board[2][j] === tap) {
-                showWinner(activePlayer);
-            } else if (board[0][0] === tap && board[1][1] === tap && board[2][2] === tap) {
-                showWinner(activePlayer);
-            } else if (board[0][2] === tap && board[1][1] === tap && board[2][0] === tap) {
-                showWinner(activePlayer);
-            };
-        }
+  let tap = players[activePlayer];
+  for (let i = 0; i <= 2; i++) {
+    for (let j = 0; j <= 2; j++) {
+     if (board[i][0] === tap && board[i][1] === tap && board[i][2] === tap) {
+        showWinner(activePlayer);
+      } else if (board[0][j] === tap && board[1][j] === tap && board[2][j] === tap) {
+        showWinner(activePlayer);
+      } else if (board[0][0] === tap && board[1][1] === tap && board[2][2] === tap) {
+        showWinner(activePlayer);
+      } else if (board[0][2] === tap && board[1][1] === tap && board[2][0] === tap) {
+        showWinner(activePlayer);
+      };
     }
+  }
 };
 
 function click(row, col) {
-    if (count % 2 == 0) {
-        activePlayer = 0;
-    } else {
-        activePlayer = 1;
-    };
-    console.log(row, col);
-    board[row][col] = players[activePlayer];
-    count++;
-    renderBoard(board);
-    checkWin();
+   if (count % 2 == 0) {
+    activePlayer = 0;
+  } else {
+    activePlayer = 1;
+  };
+  board[row][col] = players[activePlayer];
+  count++;
+  renderBoard(board);
+  checkWin();
 }
-
-startGame()
